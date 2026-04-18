4d2cafb95a101a7f2e2c90d7cdcd6511281c71c5
YisetV <vaca.yisetl@uniagustiniana.edu.co>
2026-04-18 20:08:57 +0000

    pagina1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <h1>Proyecto de Motos</h1>
+
+      <p>Bienvenido a nuestra página de motos.</p>
+
+      <h2>Modelos disponibles</h2>
+
+      <ul>
+        <li>Yamaha R1</li>
+        <li>Honda CBR600RR</li>
+        <li>Kawasaki Ninja 400</li>
+      </ul>
+
+      <p>Aquí podrás encontrar información sobre diferentes motos.</p>
+    </>
+  );
+}
+
+export default App;
